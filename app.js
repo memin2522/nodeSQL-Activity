45b931ba1db330826cde77903a9996c99cf20f15
Memo Reyes <sergio@yeltic.com>
2025-03-03 16:49:01 -0500

    Added testing function

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const SqlConnection = require('./connection');
-
 const db = new SqlConnection();
 
 async function getAllSongs() {
     try {
-        await db.connectToDb();
         const results = await db.query('SELECT * FROM song');
         console.log('Results:', results);
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+/*
+ Dentro de esta función tiene que ir los nombres de cada una de las funciones que crearon para el CRUD
+ Solamente cambien el nombre de la funcion y agreguen las lineas nuevas.
+*/
+async function testCrud(){
+    try {
+        await db.connectToDb();
+        await getAllSongs();       
     } catch (err) {
         console.error('Error:', err);
     } finally {
         await db.closeConnection();
     }
 }
-getAllSongs();
+
+testCrud();
